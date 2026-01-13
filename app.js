aa0b68f0a38a0ff56cb10625e75dd1689c5a1c87
saurabh <saurabhrajput.25072005@gmail.com>
2026-01-13 17:36:21 +0530

    fix: add Vercel URL to CORS allowed origins

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174', 
   'http://localhost:5175',
+  'https://havento.vercel.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
